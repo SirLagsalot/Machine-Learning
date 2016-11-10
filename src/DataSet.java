7d0a9d0c7a3f5a086db75ef5b303c7411287502f
WilsonSunBritten <wilsunb@gmail.com>
2016-11-09 19:27:44 -0700

    Mapper

--- src/DataSet.java
+++ src/DataSet.java
@@ -4,73 +4,14 @@ import java.util.HashMap;
 
 public class DataSet {
 
-    public ArrayList<DataLine> set;
-    private HashMap<Integer, Integer> missing;
+    public ArrayList<Integer> data;
+    Mapper map;
 
-    public DataSet() {
-        set = new ArrayList();
-        missing = new HashMap();
-    }
-
-    public void printData() {
-        for (DataLine d : set) {
-            for (int i = 0; i < d.size; i++) {
-                d.printVal(i);
-            }
-            System.out.println("");
+    public class Mapper{
+        public String[] classifications;
+        public String getClassification(int classification){
+            return classifications[classification];
         }
     }
 
-    public void addLine(String line, int index) {
-        line += "  ";
-        int i = 0;
-        line = line.replaceAll(",", " ");
-        DataLine temp = new DataLine();
-        while (!line.equals(" ")) {
-            //System.out.println(next.substring(0, next.indexOf(" ")));
-            if (!line.substring(0, line.indexOf(" ")).equals("?")) {
-                temp.addData(line.substring(0, line.indexOf(" ")), i);
-            } else {
-                missing.put(i, index);
-            }
-            line = line.substring(line.indexOf(" ") + 1);
-            i++;
-        }
-        set.add(temp);
-    }
-
-    public class DataLine {
-
-        private HashMap<Integer, String> strings;
-        private HashMap<Integer, Double> nums;
-        private int size;
-
-        public DataLine() {
-            strings = new HashMap();
-            nums = new HashMap();
-        }
-
-        public void addData(String data, int key) {
-            try {
-                double val = Double.parseDouble(data);
-                nums.put(key, val);
-                size++;
-            } catch (NumberFormatException e) {
-                strings.put(key, data);
-                size++;
-            }
-        }
-
-        public void printVal(int key) {
-            if (strings.containsKey(key)) {
-                System.out.print(strings.get(key) + " ");
-            } else {
-                System.out.print(nums.get(key) + " ");
-            }
-        }
-
-        public int getSize() {
-            return size;
-        }
-    }
 }
