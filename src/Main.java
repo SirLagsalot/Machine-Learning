1a3e280330ed385619ec90f25d3e6e1ffe121390
Zach Connelly <zmconnelly@gmail.com>
2016-11-15 08:36:29 -0700

    The struggle continues..

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         DataSet dataSet = Reader.readFile("./data/glass.data.txt");
-        Tester tester = new Tester(dataSet, "glass");
-        tester.normalize(dataSet.data);
+       // Tester tester = new Tester(dataSet, "glass");
+        //tester.normalize(dataSet.data);
 //        dataSet = Reader.readFile("./data/breast-cancer-wisconsin.data.txt");
-//        tester = new Tester(data, "breast-cancer-wisconsin");
+//        tester = new Tester(dataSet, "breast-cancer-wisconsin");
 //
 //        dataSet = Reader.readFile("./data/house-votes-84.data.txt");
-//        tester = new Tester(data, "house-votes-84");
+//        tester = new Tester(dataSet, "house-votes-84");
 //
 //        dataSet = Reader.readFile("./data/iris.data.txt");
-//        tester = new Tester(data, "iris");
+//        tester = new Tester(dataSet, "iris");
 //
-//        dataSet = Reader.readFile("./data/soybean-small.data.txt");
-//        tester = new Tester(data, "soybean-small");
+        dataSet = Reader.readFile("./data/soybean-small.data.txt");
+        Tester tester = new Tester(dataSet, "soybean-small");
     }
 }
