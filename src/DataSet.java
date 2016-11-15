3dcfb854e9074f1cb92fcbe2324d2f7db92ab774
Zach Connelly <zmconnelly@gmail.com>
2016-11-15 10:15:55 -0700

    Changes to reader and data files
    pls no conflicts..

--- src/DataSet.java
+++ src/DataSet.java
@@ -3,16 +3,24 @@ import java.util.ArrayList;
 
 public class DataSet {
 
-    public ArrayList<Instance> data = new ArrayList();
-    private ArrayList<String> classifications;
+    public ArrayList<Instance> data = new ArrayList<>();
+    private ArrayList<String> classifications = new ArrayList<>();
     public int length;
-    
+
+    public void addLine(Instance newInstance, String classification) {
+
+        data.add(newInstance);
+        classifications.add(classification);
+    }
+
     public String getClassification(int classID) {
+
         return classifications.get(classID);
     }
-    
+
     public void addClassification(String addClass) {
+
         classifications.add(addClass);
     }
-    
+
 }
--- src/Instance.java
+++ src/Instance.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 
-public class Instance implements Iterable, Comparator {
+public class Instance implements Comparator {
 
     protected int classification;
     protected ArrayList<Integer> features;
@@ -17,11 +17,6 @@ public class Instance implements Iterable, Comparator {
         this.discrete = false;
         this.distance = 0.0f;
         this.classification = -1;
-    }
-    
-    @Override
-    public Iterator iterator() {
-        return features.iterator();
     }
 
     @Override
