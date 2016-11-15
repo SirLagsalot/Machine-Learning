f47509da5102193c56d7bf015dec36efe6e8182e
Zach Connelly <zmconnelly@gmail.com>
2016-11-15 16:55:00 -0700

    Meow

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,27 @@
 
 import java.util.ArrayList;
 
-
 public class Main {
 
     public static void main(String[] args) {
 
-//        DataSet dataSet = Reader.readFile("./data/glass.data.txt");
-       // Tester tester = new Tester(dataSet, "glass");
-        //tester.normalize(dataSet.data);
-//        dataSet = Reader.readFile("./data/breast-cancer-wisconsin.data.txt");
-//        tester = new Tester(dataSet, "breast-cancer-wisconsin");
-//
-//        dataSet = Reader.readFile("./data/house-votes-84.data.txt");
-//        tester = new Tester(dataSet, "house-votes-84");
-//
-//        dataSet = Reader.readFile("./data/iris.data.txt");
-//        tester = new Tester(dataSet, "iris");
-//
-//        DataSet dataSet = Reader.readFile("./data/soybean-small.data.txt");
-//        Tester tester = new Tester(dataSet, "soybean-small");
-        ID3 id3 = new ID3(new ArrayList());
-//        DataSet dataSet;
-//        Tester tester;
-//
-//        dataSet = Reader.readFile("./data/glass.data.txt");
-//        tester = new Tester(dataSet, "glass");
-//        
-//        dataSet = Reader.readFile("./data/breast-cancer-wisconsin.data.txt");
-//        tester = new Tester(dataSet, "breast-cancer-wisconsin");
-//
-//        dataSet = Reader.readFile("./data/house-votes-84.data.txt");
-//        tester = new Tester(dataSet, "house-votes-84");
-//
-//        dataSet = Reader.readFile("./data/iris.data.txt");
-//        tester = new Tester(dataSet, "iris");
-//
-//        dataSet = Reader.readFile("./data/soybean-small.data.txt");
-//        tester = new Tester(dataSet, "soybean-small");
+        DataSet dataSet;
+        Tester tester;
+
+        dataSet = Reader.readFile("./data/glass.data.txt");
+        tester = new Tester(dataSet, "glass");
+
+        dataSet = Reader.readFile("./data/breast-cancer-wisconsin.data.txt");
+        tester = new Tester(dataSet, "breast-cancer-wisconsin");
+
+        dataSet = Reader.readFile("./data/house-votes-84.data.txt");
+        tester = new Tester(dataSet, "house-votes-84");
+
+        dataSet = Reader.readFile("./data/iris.data.txt");
+        tester = new Tester(dataSet, "iris");
+
+        dataSet = Reader.readFile("./data/soybean-small.data.txt");
+        tester = new Tester(dataSet, "soybean-small");
+        //ID3 id3 = new ID3(new ArrayList());
     }
 }
