e6efbe547297943d3f27170efdf0208062e4f2db
WilsonSunBritten <wilsunb@gmail.com>
2016-11-15 18:02:11 -0700

    small fixes

--- src/Utilities.java
+++ src/Utilities.java
@@ -7,6 +7,7 @@ public class Utilities {
 
     public static int getClassificationCount(ArrayList<Instance> dataSet) {
         Set set = new HashSet<>();
+        int oldSize = 0;
         for (Instance instance : dataSet) {
             set.add(instance.classification);
         }
