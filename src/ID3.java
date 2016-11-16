5343bccf5638ea6097fe00df79535e9cda17cc77
Ryan Brand <UNA8949@gmail.com>
2016-11-15 21:38:34 -0700

    Comments

--- src/ID3.java
+++ src/ID3.java
@@ -8,42 +8,43 @@ public class ID3 implements Classifier {
     private final ArrayList<Instance> trainingData;
 
     private ArrayList<Integer[]> test = new ArrayList();
+    private Tree decisionTree;
 
     public ID3(ArrayList<Instance> trainingData) {
 
         this.trainingData = trainingData;
-        ArrayList<Integer[]> testSet = new ArrayList();
-
-        Integer[] test2 = {0, 0};
-        Integer[] test3 = {1, 0};
-        Integer[] test4 = {0, 1};
-        Integer[] test5 = {0, 1};
-        Integer[] test6 = {0, 1};
-        Integer[] test7 = {1, 0};
-        Integer[] test8 = {1, 1};
-        Integer[] test9 = {0, 0};
-        Integer[] test10 = {0, 1};
-        Integer[] test11 = {0, 1};
-        Integer[] test12 = {1, 1};
-        Integer[] test13 = {1, 1};
-        Integer[] test14 = {0, 1};
-        Integer[] test15 = {1, 0};
-
-        testSet.add(test2);
-        testSet.add(test3);
-        testSet.add(test4);
-        testSet.add(test5);
-        testSet.add(test6);
-        testSet.add(test7);
-        testSet.add(test8);
-        testSet.add(test9);
-        testSet.add(test10);
-        testSet.add(test11);
-        testSet.add(test12);
-        testSet.add(test13);
-        testSet.add(test14);
-        testSet.add(test15);
-        System.out.println(gain(testSet));
+//        ArrayList<Integer[]> testSet = new ArrayList();
+//
+//        Integer[] test2 = {0, 0};
+//        Integer[] test3 = {0, 0};
+//        Integer[] test4 = {1, 1};
+//        Integer[] test5 = {2, 1};
+//        Integer[] test6 = {2, 1};
+//        Integer[] test7 = {2, 0};
+//        Integer[] test8 = {1, 1};
+//        Integer[] test9 = {0, 0};
+//        Integer[] test10 = {0, 1};
+//        Integer[] test11 = {2, 1};
+//        Integer[] test12 = {0, 1};
+//        Integer[] test13 = {1, 1};
+//        Integer[] test14 = {1, 1};
+//        Integer[] test15 = {2, 0};
+//
+//        testSet.add(test2);
+//        testSet.add(test3);
+//        testSet.add(test4);
+//        testSet.add(test5);
+//        testSet.add(test6);
+//        testSet.add(test7);
+//        testSet.add(test8);
+//        testSet.add(test9);
+//        testSet.add(test10);
+//        testSet.add(test11);
+//        testSet.add(test12);
+//        testSet.add(test13);
+//        testSet.add(test14);
+//        testSet.add(test15);
+//        System.out.println(gain(testSet));
     }
 
     @Override
@@ -52,22 +53,30 @@ public class ID3 implements Classifier {
         return -1;
     }
 
+    public void findRootNode() {
+        
+    }
+
+    //Information gain calculation
     public double gain(ArrayList<Integer[]> vals) {
+        //List declarations
         ArrayList<Integer> classVals = new ArrayList();
         ArrayList<Integer> attrVals = new ArrayList();
         ArrayList<Integer> unique;
+        
+        //Separate the input list
         for (Integer[] i : vals) {
             attrVals.add(i[0]);
             classVals.add(i[1]);
         }
 
-        unique = getUniqueAttrValues(attrVals);
+        unique = getUniqueAttrValues(attrVals); //Get unique values
         int sum = vals.size();
 
-        double gain = entropy(countClass(classVals));
+        double gain = entropy(countClass(classVals)); // Entropy(S)
         ArrayList<Integer> tempClasses;
 
-        for (Integer i : unique) {
+        for (Integer i : unique) { // - (sumation(|sv|/|s|) * Entropy(sv))
             tempClasses = getClassCountForIndAttr(i, vals);
             gain -= (((double) countAttr(i, vals) / sum) * entropy(tempClasses));
         }
@@ -75,10 +84,11 @@ public class ID3 implements Classifier {
         return gain;
     }
 
+    //Count number of classifications for an individual value of an attribute
     public ArrayList<Integer> getClassCountForIndAttr(Integer attr, ArrayList<Integer[]> vals) {
-        HashMap<Integer, Integer> tempCount = new HashMap();
-        for (Integer[] i : vals) {
-            if (attr.equals(i[0])) {
+        HashMap<Integer, Integer> tempCount = new HashMap(); //Used to obtain only unique values
+        for (Integer[] i : vals) { // add a unique value or increment counter on existing
+            if (attr.equals(i[0])) { // if the index equals the value of the attribute we're looking for
                 if (tempCount.containsKey(i[1])) {
                     tempCount.put(i[1], tempCount.get(i[1]) + 1);
                 } else {
@@ -89,7 +99,7 @@ public class ID3 implements Classifier {
 
         Iterator it = tempCount.entrySet().iterator();
         ArrayList<Integer> count = new ArrayList();
-        while (it.hasNext()) {
+        while (it.hasNext()) { //Edit out the key for returning only values
             HashMap.Entry pair = (HashMap.Entry) it.next();
             count.add((Integer) pair.getValue());
             it.remove();
@@ -97,15 +107,17 @@ public class ID3 implements Classifier {
         return count;
     }
 
+    //Entropy calculation
     public double entropy(ArrayList<Integer> portions) {
         double entropy = 0;
         int total = sum(portions);
         for (Integer i : portions) {
-            entropy += (0 - (((double) i / total) * (Math.log((double) i / total) / Math.log(2))));
+            entropy += (0 - (((double) i / total) * (Math.log((double) i / total) / Math.log(2)))); //Math.log(2) used to convert to log2 from log10
         }
         return entropy;
     }
 
+    //Get all uniqe values for specific attribute
     public ArrayList<Integer> getUniqueAttrValues(ArrayList<Integer> vals) {
         ArrayList<Integer> unique = new ArrayList();
         for (Integer i : vals) {
@@ -116,6 +128,7 @@ public class ID3 implements Classifier {
         return unique;
     }
 
+    //Count the number of a specific attribute in the dataset
     public Integer countAttr(Integer val, ArrayList<Integer[]> vals) {
         Integer count = 0;
         for (Integer[] i : vals) {
@@ -126,18 +139,20 @@ public class ID3 implements Classifier {
         return count;
     }
 
+    //Count the number of classifications for gain
     public ArrayList<Integer> countClass(ArrayList<Integer> vals) {
         ArrayList<Integer> res = new ArrayList();
-        HashMap<Integer, Integer> temp = new HashMap();
-        for (Integer i : vals) {
+        HashMap<Integer, Integer> temp = new HashMap(); //Used for unique values
+        for (Integer i : vals) { // add a unique value or increment counter on existing
             if (temp.containsKey(i)) {
                 temp.put(i, temp.get(i) + 1);
             } else {
                 temp.put(i, 1);
             }
         }
+        
         Iterator it = temp.entrySet().iterator();
-        while (it.hasNext()) {
+        while (it.hasNext()) { //Edit out the key for returning only values
             HashMap.Entry pair = (HashMap.Entry) it.next();
             res.add((Integer) pair.getValue());
             it.remove();
@@ -145,6 +160,7 @@ public class ID3 implements Classifier {
         return res;
     }
 
+    //Find the total number of values for entropy/gain
     public int sum(ArrayList<Integer> vals) {
         int sum = 0;
         for (int i = 0; i < vals.size(); i++) {
@@ -152,5 +168,27 @@ public class ID3 implements Classifier {
 
         }
         return sum;
+    }
+
+    //Tree class
+    private class Tree {
+        Node root;
+        public Tree(Node root){
+            this.root = root;
+        }
+        
+        //Print the tree for testing
+        public void printTree(){
+            
+        }
+        
+    }
+    
+    //Node with varying number of children
+    private class Node{
+        boolean isLeaf;
+        boolean isAttr;
+        int path;
+        ArrayList<Node> children;
     }
 }
